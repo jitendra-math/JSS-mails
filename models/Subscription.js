c98350543c4ee26fe8ebec61f72f582298edbcd3
Jitendra Singh Nimod <i.jitendra.singh0@gmail.com>
2026-03-01 13:20:48 +0530

    Update Subscription.js

--- models/Subscription.js
+++ models/Subscription.js
@@ -1,22 +1,26 @@
 import mongoose from "mongoose";
 
 const SubscriptionSchema = new mongoose.Schema({
-  // Taki humein pata rahe ki ye kis user ka phone/browser hai
+  // User ki pehchaan (Agar login system hai toh User ID yahan aayegi)
   userId: { 
-    type: mongoose.Schema.Types.ObjectId, 
-    ref: "User", 
-    required: true 
+    type: String, 
+    required: true,
+    default: "jss_admin_user" 
   },
-  // Browser ka banaya hua token object
+  
+  // Ye sabse main token/address hai jo browser generate karke dega
+  // Isike andar public key aur endpoint url chhupe hote hain
   subscription: { 
     type: Object, 
     required: true 
   },
-  // Konsa device hai (Chrome, Safari, iOS, etc.)
+  
+  // Ye bas details ke liye hai ki user ne iPhone se allow kiya ya Laptop se
   userAgent: { 
     type: String,
     default: "Unknown Device"
   }
 }, { timestamps: true });
 
+// Agar model pehle se bana hai toh wahi use karo, varna naya bana do
 export default mongoose.models.Subscription || mongoose.model("Subscription", SubscriptionSchema);
