74dcce2e89972b75a3d8032482d4db58d386b6c5
Jitendra Singh Nimod <i.jitendra.singh0@gmail.com>
2026-03-03 12:20:06 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,19 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#007AFF",   // iPhone blue
-        softbg: "#f5f5f7"
+        primary: "#007AFF"
+      },
+      backgroundImage: {
+        softbg: `
+          radial-gradient(circle at 20% 10%, rgba(255,255,255,0.7), transparent 40%),
+          linear-gradient(
+            180deg,
+            #faf7f2 0%,
+            #f1e6d6 35%,
+            #e6cfad 70%,
+            #d9b98c 100%
+          )
+        `
       },
       borderRadius: {
         xl2: "1.25rem"
